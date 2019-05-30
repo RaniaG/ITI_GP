f882f51c74351ab5ff06e57de39833bf525f3623
BassantFahmy <bassantfahmy@gmail.com>
2019-05-30 13:21:36 +0200

    homepage

--- src/app/Core/core.module.ts
+++ src/app/Core/core.module.ts
@@ -3,16 +3,22 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, NavbarComponent],
+  declarations: [FooterComponent, HeaderComponent, NavbarComponent, LoginComponent, SignupComponent, HomeComponent],
   imports: [
     CommonModule
   ],
    exports: [
     FooterComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ]
 })
 export class CoreModule { }
