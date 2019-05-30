b9fd1d77c047a18bb75d1d9c64bce930bdd62cd0
BassantFahmy <bassantfahmy@gmail.com>
2019-05-30 14:49:21 +0200

    profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './Core/core.module';
+import { NgbModule, NgbRating } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ShopModule } from './Features/shop/shop.module';
+import { ProfileModule } from './Features/profile/profile.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { ShopModule } from './Features/shop/shop.module';
   imports: [
     BrowserModule,
     ShopModule,
-    CoreModule
+    CoreModule,
+    ProfileModule,
+    NgbModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
