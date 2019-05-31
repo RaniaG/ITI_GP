f3314f6caec19f520cc073df168ac8a906291d2e
DoaaOsama <doaa_kandeel@hotmail.com>
2019-05-31 02:08:34 +0200

    added ad-edit product & related products components

--- src/app/Features/product/product.module.ts
+++ src/app/Features/product/product.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { SharedModule } from 'src/app/Shared/shared.module';
+import { RelatedProductsComponent } from './related-products/related-products.component';
+import { AddEditProductComponent } from './add-edit-product/add-edit-product.component';
 
 @NgModule({
-  declarations: [ProductCardComponent, ProductListComponent],
+  declarations: [ProductCardComponent, ProductListComponent, RelatedProductsComponent, AddEditProductComponent],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
     ProductCardComponent,
-    ProductListComponent
+    ProductListComponent,
+    RelatedProductsComponent,
+    AddEditProductComponent
   ]
 })
 export class ProductModule { }
