0d5444359ac5bc9ecec4f76609e757363b6b2180
BassantFahmy <bassantfahmy@gmail.com>
2019-06-01 03:31:11 +0200

    editprofile validations using reactiveforms (unfinished)

--- src/app/Features/profile/edit-profile/edit-profile.component.ts
+++ src/app/Features/profile/edit-profile/edit-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormArray, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
@@ -8,8 +8,45 @@ import { Component, OnInit } from '@angular/core';
 export class EditProfileComponent implements OnInit {
 
   constructor() { }
-
+  editProfileForm: FormGroup;
+  validator: ValidatorFn;
   ngOnInit() {
+    this.editProfileForm = new FormGroup({
+      'firstname': new FormControl('bassant', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*')]),
+      'lastname': new FormControl('Mohamed', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*')]),
+      'username': new FormControl('bassant fahmy', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*')]),
+      'email': new FormControl('bassantfahmy@gmail.com', [Validators.required, Validators.email]),
+      'changePassword': new FormControl('12345678'),
+      'confirmPassword': new FormControl('12345678')
+      // 'confirmPassword': new FormControl('12345678', this.MustMatch('changePassword', 'confirmPassword'))
+    });
   }
+  onSubmit(editProfileForm) {
+    console.log(editProfileForm);
+  }
+  MustMatch(control1: string, control2: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      let result: ValidationErrors;
+      if (control.get(control1).value !== control.get(control2).value) {
+        result = { compare: true };
+      }
+      else {
+        result = null;
+      }
+      return result;
+    }
+  }
+  // MustMatch=(control1: string, control2: string): ValidatorFn =>{
+  //   return (control: AbstractControl): ValidationErrors | null => {
+  //     let result: ValidationErrors;
+  //     if (control.get(control1).value !== control.get(control2).value) {
+  //       result = { compare: true };
+  //     }
+  //     else {
+  //       result = null;
+  //     }
+  //     return result;
+  //   }
+  // }
 
 }
--- src/app/Features/profile/profile.module.ts
+++ src/app/Features/profile/profile.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProfileComponent, EditProfileComponent],
   imports: [
     CommonModule,
     NgbModule,
+    ReactiveFormsModule,
+    
     RouterModule.forChild([
       { path: 'profile', component: ProfileComponent },
       { path: 'editprofile', component: EditProfileComponent },
