20910f1184f0b9e6b51fbd09998caccb673193bd
hadeer-attalluh <hadeermohamed.fcis@gmail.com>
2019-06-02 11:16:35 +0200

    finish summary-card component , add it to dashboard-listing

--- src/app/Features/dashboard/summary-card/summary-card.component.ts
+++ src/app/Features/dashboard/summary-card/summary-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-summary-card',
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./summary-card.component.scss']
 })
 export class SummaryCardComponent implements OnInit {
+  @Input('bg-color')
+  cardColor: string;
+  @Input('imageUrl')
+  cardImage: string;
+  @Input('value')
+  cardValue: string;
+  @Input('label')
+  cardLabel: string;
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
