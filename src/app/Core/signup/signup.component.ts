efd9122c97b327b4eb043989d3bd4a8fe14329eb
Asma Soliman <msoliman.asmaa@gmail.com>
2019-06-20 13:57:13 +0200

    sign-up form validations

--- src/app/Core/signup/signup.component.ts
+++ src/app/Core/signup/signup.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl,Validators,ValidatorFn,ValidationErrors} from '@angular/forms';
 import { CountryCityService } from 'src/app/_service/country-city.service';
 import { Country, City } from 'src/app/_models/country-city';
+import { UserService } from 'src/app/_service/user.service';
+import { from } from 'rxjs';
+import { User } from 'src/app/_models/user';
+
 
 @Component({
   selector: 'app-signup',
@@ -13,31 +17,29 @@ export class SignupComponent implements OnInit {
   signUpForm : FormGroup;
   countries :Country[]; 
   cities : City[];
+  user : User;
 
-  constructor(private countryCityService: CountryCityService) { 
+  constructor(private countryCityService: CountryCityService,private userService : UserService) { 
 
 
   }
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      txtFN : new FormControl(),
-      txtLN : new FormControl(),
-      txtUN : new FormControl(),
-      phoneNumber: new FormControl(),
-      email :new FormControl(),
-      password : new FormControl(),
-      confirmPW : new FormControl(),
+      txtFN : new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*')]),
+      txtLN : new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*')]),
+      txtUN : new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*')]),
+      phoneNumber: new FormControl('',[Validators.required]),
+      email :new FormControl('',[Validators.required, Validators.email]),
+      password : new FormControl('',[Validators.required,Validators.pattern('^(?=.*\d).{4,8}$')]),
+      confirmPW : new FormControl('',[Validators.required]),
       txtStreet : new FormControl(),
       txtBuilding: new FormControl(),
       txtApartment : new FormControl(),
       country :new FormControl(),
       city: new FormControl(),
       gender: new FormControl('Male'),
-
-
-
-      
+       
     })
        
           this.countries = this.countryCityService.getAllCountries();
@@ -55,7 +57,10 @@ export class SignupComponent implements OnInit {
 
 
   onSubmit(){
-         console.log(this.signUpForm);
+         if(this.signUpForm.valid){
+               this.user = this.signUpForm.value;
+               this.userService.add(this.user);
+         }
   }
 
 }
