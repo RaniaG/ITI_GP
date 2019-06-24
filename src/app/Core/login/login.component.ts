926d3e069b4ecb19e01fdde633ff362eaf5a1823
Asma Soliman <msoliman.asmaa@gmail.com>
2019-06-24 21:16:39 +0200

    user authentication

--- src/app/Core/login/login.component.ts
+++ src/app/Core/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +12,43 @@ import { FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   logInForm : FormGroup
-  
-  constructor() { }
+  users : User[];
+  currentUser : User;
+  emailExists : boolean;
+  validPassword: string;
+  incorrectPassword: boolean;
+
+  constructor(private userService : UserService,private router :Router) { }
 
   ngOnInit() {
     this.logInForm = new FormGroup({
-      
+      email : new FormControl(),
+      password: new FormControl(),
     })
+    this.users = this.userService.getAll();
     
+  }
+
+  onSubmit(){
+       this.currentUser= this.logInForm.value;
+       this.users.forEach(user=>{
+               if(user.email == this.currentUser.email)
+               {
+                 this.validPassword = user.password;
+                 
+               }
+               else{
+                 this.emailExists=true;
+               }
+               if(this.currentUser.password == this.validPassword)
+               {
+                this.router.navigate(['/home'])
+               }
+               else{
+                 this.incorrectPassword= true;
+               }
+       })
+      
   }
 
 }
