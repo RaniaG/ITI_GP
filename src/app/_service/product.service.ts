57a0c667ab449e9a7d6c54c9d4e0ed10eb0a5668
oMnia-Elsayed <omniamorsi2020@gmail.com>
2019-06-24 23:52:42 +0200

    add product validators - related products

--- src/app/_service/product.service.ts
+++ src/app/_service/product.service.ts
@@ -37,6 +37,15 @@ export class ProductService {
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
                 terms: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi quo incidunt quidem rerum mollitia in",
                 category: { id: 3, name: "Decor" }
+            },
+            {
+                id: 4,
+                name: 'Product 4',
+                price: 50, discount: 3, quantity: 5,
+                images: ['assets/images/image_11-1-300x300.jpg', 'assets/images/image_52-300x300.jpg'],
+                description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                terms: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi quo incidunt quidem rerum mollitia in",
+                category: { id: 3, name: "Decor" }
             }
             // {id: "4" , name: 'Product 4' , price: 20 , discount: 2 , images:['assets/images/image_11-1-300x300.jpg','assets/images/image_52-300x300.jpg']},
             // {id: "5" , name: 'Product 5' , price: 30 , discount: 2.5 , images:['assets/images/image_11-1-300x300.jpg','assets/images/image_52-300x300.jpg']},
@@ -68,6 +77,10 @@ export class ProductService {
 
 
     getRelated(id: number): Product[] {
-        return this.products;
+        const categoryId = this.products.find (e => e.id == id).category.id
+        // console.log(c)
+        let related = this.products.filter( a => a.category.id == categoryId)
+        // console.log(x)
+        return related;
     }
 }
