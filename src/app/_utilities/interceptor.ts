f2746499642660bc55bdadc21c3bde35f76556ec
RaniaG <raniagml12@gmail.com>
2019-06-25 19:16:49 +0200

    created init service and updated auth service

--- src/app/_utilities/interceptor.ts
+++ src/app/_utilities/interceptor.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from '../_auth/auth.service';
+import { AppInitService } from '../_service/app-init.service';
 
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private appinit: AppInitService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         debugger;
-        if (this.authService.isAuthenticated()) {
+        if (this.authService.isAuthenticated() || this.appinit.token) {
             request = request.clone({
                 setHeaders: {
-                    Authorization: `Bearer ${this.authService.token}`
+                    Authorization: `Bearer ${this.appinit.token}`
                 }
             });
         }
